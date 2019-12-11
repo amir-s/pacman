2c8b456d3a4c26570236d061a674a0cd873f5510
Amir Saboury <amir.saboury@gmail.com>
2019-12-11 10:09:01 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+const UNIT = 50;
+const map = `
+###############
+###############
+##           ##
+##    ##     ##
+##    ##     ##
+##           ##
+###############
+###############
+`
+  .split("\n")
+  .slice(1, -1)
+  .map(line => line.split(""));
+
+const border = (i, j) => {
+  if (i < 0 || j < 0) return false;
+  if (i >= map.length || j >= map[0].length) return false;
+  return map[i][j] === "#";
+};
 
 const App: React.FC = () => {
+  const cells = [];
+  for (let i = 0; i < map.length - 0; i++) {
+    for (let j = 0; j < map[i].length - 0; j++) {
+      const style = {
+        width: `${UNIT}px`,
+        height: `${UNIT}px`
+      };
+
+      const borders = [];
+      if (map[i][j] === "#") {
+        const offset = [-1, 0, 1];
+        const counts = [
+          offset.map(c => !border(i - 1, j + c)).filter(i => i).length,
+          offset.map(c => !border(i + c, j + 1)).filter(i => i).length,
+          offset.map(c => !border(i + 1, j + c)).filter(i => i).length,
+          offset.map(c => !border(i + c, j - 1)).filter(i => i).length
+        ];
+        const straights = counts.map(c => c > 1);
+        if (straights.filter(i => i).length === 1) {
+          const type = straights.indexOf(true) % 2 === 0 ? "h" : "v";
+          borders.push(
+            <div
+              key={`cell-border-${i}-${j}`}
+              className={`wall wall-${type}`}
+            ></div>
+          );
+        } else if (straights.filter(i => i).length === 2) {
+          let type = "";
+          if (straights[0] && straights[1]) {
+            type = "ur";
+          } else if (straights[1] && straights[2]) {
+            type = "rb";
+          } else if (straights[2] && straights[3]) {
+            type = "bl";
+          } else if (straights[3] && straights[0]) {
+            type = "lu";
+          }
+          borders.push(
+            <div
+              key={`cell-border-${i}-${j}`}
+              className={`wall wall-${type}`}
+            ></div>
+          );
+        } else {
+          let type = "";
+          if (counts[0] === 1 && counts[1] === 1) {
+            type = "bl";
+          } else if (counts[1] === 1 && counts[2] === 1) {
+            type = "lu";
+          } else if (counts[2] === 1 && counts[3] === 1) {
+            type = "ur";
+          } else if (counts[3] === 1 && counts[0] === 1) {
+            type = "rb";
+          }
+          borders.push(
+            <div
+              key={`cell-border-${i}-${j}`}
+              className={`wall wall-${type}`}
+            ></div>
+          );
+        }
+      }
+
+      cells.push(
+        <div key={`cell-${i}-${j}`} className="cell" style={style}>
+          {borders}
+        </div>
+      );
+    }
+  }
+  const style = {
+    width: `${map[0].length * UNIT}px`,
+    height: `${map.length * UNIT}px`
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={style}>
+      {cells}
     </div>
   );
-}
+};
 
 export default App;
